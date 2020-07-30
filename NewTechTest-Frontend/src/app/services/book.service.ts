bf5bdf2272fc5adda09d9dc98e83fd08d6539623
Jesus Estevez <ing.estevezaquino@gmail.com>
2020-07-30 11:15:33 -0400

    Se realizaron cambios minusculos en las url de los endpoints(BookService - Frontend).

--- NewTechTest-Frontend/src/app/services/book.service.ts
+++ NewTechTest-Frontend/src/app/services/book.service.ts
@@ -9,22 +9,22 @@ export class BookService {
   constructor(private client:HttpClient) {}
 
   GetBooks(){
-    return this.client.get("http://booksapi.somee.com/api/books");
+    return this.client.get("https://localhost:44334/api/books");
   }
 
   GetBookById(id:number){
-    return this.client.get("http://booksapi.somee.com/api/books/"+id);
+    return this.client.get("https://localhost:44334/api/books/"+id);
   }
 
   AddBook(book:any){
-    return this.client.post("http://booksapi.somee.com/api/books", book);
+    return this.client.post("https://localhost:44334/api/books", book);
   }
 
   EditBook(id:number, book:any){
-    return this.client.put("http://booksapi.somee.com/api/books/"+id, book);
+    return this.client.put("https://localhost:44334/api/books/"+id, book);
   }
 
   DeleteBook(id:number){
-    return this.client.delete("http://booksapi.somee.com/api/books/"+id);
+    return this.client.delete("https://localhost:44334/api/books/"+id);
   }
 }
